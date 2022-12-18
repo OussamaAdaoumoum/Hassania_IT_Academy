9a9f29eb6e951eec814a836789699329aa0802a9
oussama adaoumoum <oussamaadm755@gmail.com>
2022-12-18 23:44:59 +0100

    add home page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import SignUp from './routes/SignUp/SignUp';
 import FormationCard from './components/FormationCard/FormationCard';
 import { Routes, Route } from "react-router-dom";
+import Home from './routes/Home/Home';
 
 
 function App() {
   return (
     <div className="App">
-    <FormationCard />
+    {/* <FormationCard /> */}
+    <Home />
     </div>
   );
 }
